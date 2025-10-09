634d7488c118967e6af22ac623e329566c9a16fa
taresh2504 <taresh25202@gmail.com>
2025-10-09 13:01:59 +0530

    h.w

--- events.js
+++ events.js
@@ -21,3 +21,20 @@ function o(){
 function color(cl){
     document.body.style.backgroundColor=cl
 }
+
+function fun1(){
+    document.getElementById('head1').textContent='changed by js'
+}
+
+// H.W
+var i = 1
+function add(){
+    var a =  document.getElementById('zero').innerText=i++
+    // a.innerHTML=i++
+}
+
+function sub(){
+   var a = document.getElementById('zero').innerText=i--
+//    a.innerHTML=--i
+}
+
