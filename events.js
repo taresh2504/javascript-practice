86abd4cf0b6b0cb8fc35a6b44046b9a16c1939f5
taresh2504 <taresh25202@gmail.com>
2025-10-09 16:26:19 +0530

    events in js

--- events.js
+++ events.js
@@ -27,14 +27,45 @@ function fun1(){
 }
 
 // H.W
-var i = 1
+var i = 0
 function add(){
-    var a =  document.getElementById('zero').innerText=i++
-    // a.innerHTML=i++
+    var a =  document.getElementById('zero').innerText=++i
 }
 
 function sub(){
-   var a = document.getElementById('zero').innerText=i--
-//    a.innerHTML=--i
+    if (i>0){
+   var a = document.getElementById('zero').innerText=--i
+    }
+    else{
+        alert("not less than 0")
+    }
 }
+
+function change(){
+  document.getElementById('btn').textContent = "get lost"
+  document.getElementById('btn').style.backgroundColor='red'
+}
+
+function change1(){
+  document.getElementById('btn').textContent = "tap here"
+  document.getElementById('btn').style.backgroundColor='green'
+}
+
+function mob(){
+  var a =  document.getElementById('mob')
+  a.setAttribute=('mobile.jpeg','height : 200px')
+}
+
+function lap(){
+  var b =  document.getElementById('lap')
+  b.setAttribute=('laptop.png','height : 200px')
+}
+
+function tab(){
+  var c =  document.getElementById('tab')
+  c.setAttribute=('tablet.jpeg','height : 200px')
+}
+
+
+
 
