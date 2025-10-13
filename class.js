c07ab5ab871d9bb38dc75a76d85737fc38ed9fd6
taresh2504 <taresh25202@gmail.com>
2025-10-13 16:32:23 +0530

    toggle

---
+++ class.js
@@ -0,0 +1,28 @@
+function fun(){
+let wrapper = document.querySelector('#container')
+wrapper.classList.add('wrapper')
+}
+
+function remov(){
+    let container = document.querySelector('#container')
+    container.classList.remove('wrapper')
+}
+
+function both(){
+    
+}
+
+
+
+function ch(){
+    document.body.classList.toggle('color')
+    let cn = document.querySelector('#container')
+    cn.classList.toggle('wrapper')
+    if (cn==true){
+       document.getElementById('dom').textContent='dark'
+    }
+    else{
+       document.getElementById('dom')
+    }
+}
+
