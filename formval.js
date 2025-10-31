0c0e64ec3ee03a2d0a42929d255ef8acd8539ccd
taresh2504 <taresh25202@gmail.com>
2025-10-31 16:24:12 +0530

    validation

--- formval.js
+++ formval.js
@@ -44,17 +44,57 @@ function validate(){
 
 let age2 = document.querySelector('#eage').value
 let contact = document.querySelector('#ectn').value
+let mail = document.querySelector('#mail').value
+let pass = document.querySelector('#pass').value
+let cpass = document.querySelector('#cpass').value
+
 
 function fun2(){
-    if (age2 >=0 || age2 >=3 ){
+    if (age2.length < 0 || age2.length > 3){
         alert('please fill your age')
         document.querySelector('#age2').focus()
         return false
     }
+    
+    // if (isNaN(age2) ){
+    //     alert('please enter age in numbers')
+    //     document.querySelector('#age2').focus
+    // }
 
     if(contact <10 || contact >10){
         alert('enter your correct contact detail')
         document.querySelector('#ectn').focus()
         return false
     }
+
+    if (mail = ''){
+        alert('please enter mail')
+        document.querySelector('#mail').focus()
+        return false
+    }
+
+    else if( !(mail.includes('@gmail.com'))){
+        alert('please enter @gmail.com in email')
+        document.querySelector('#mail').focus()
+        return false
+    }
+
+    if (pass == ''){
+        alert('enter password')
+        document.querySelector('#pass').focus()
+        return false
+    }
+
+    else if( !(pass.match(/[~``!@#$%^&*()]/))){
+        alert('please enter special charcater')
+        document.querySelector('#pass').focus()
+        return false
+    }
+
+    else if( !(pass.match(/[1234567890]/))){
+        alert('please enter one number')
+        document.querySelector('#pass').focus()
+        return false
+    }
+    
 }
